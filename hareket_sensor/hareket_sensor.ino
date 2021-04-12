d33eb374b240f7abf5ebf07b53b08b22f412403d
Farukeness <80718763+Farukeness@users.noreply.github.com>
2021-04-13 00:23:45 +0300

    Add files via upload

---
+++ hareket_sensor/hareket_sensor.ino
@@ -0,0 +1,28 @@
+#define trigger_pin = 7;
+#define echo_pin = 6;
+
+int sure;
+int mesafe;
+
+void setup() {
+  pinMode(trigger_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  //trigger pine komut göndererek ses dalgası gönderiyoruz  
+  digitalWrite(trigger_pin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigger_pin, LOW);
+  //pulseIn komutu echo_pine gelen HIGH komutundan tekrar LOW olana kadar olan süreyi bize veriyor bizde bunu sure değişkenimize aktarıyoruz
+  sure = pulseIn(echo_pin, HIGH);
+  // pulseIn komutuyla elde ettiğimiz ses dalgası hızını basit bir hesapla mesafeye çeviriyoruz.
+  mesafe = (sure/2) / 29.1;
+
+  Serial.print("Mesafe = ");
+  Serial.println(mesafe);
+  delay(100);
+
+}
